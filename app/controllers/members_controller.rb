2f3a1bec38e1b9d0a31621348aae69ef2ee8588b
theodorik comlan <comlantheodorik@gmail.com>
2024-06-06 14:57:23 +0200

    some change dashboard, member, show, activities

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,8 @@ class MembersController < ApplicationController
     @activity = Activity.find(params[:activity_id])
     @member = Member.new
     @member.activity = @activity
+    @member.accepted = false
+
     @member.user = current_user
 
     if @member.save!
