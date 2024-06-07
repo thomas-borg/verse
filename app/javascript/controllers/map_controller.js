f481e74cf6ae8dc37608c4486e2169a1d3385189
Lucas Silva <lucasilrs@outlook.com>
2024-06-07 12:21:07 +0200

    Finding location on map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,7 +15,6 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
   }
