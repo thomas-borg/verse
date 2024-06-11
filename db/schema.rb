309e6b7f90dcda792be6e8136c7b410bc066bb1c
Ahmed Musa <yilmazahmet1994@gmail.com>
2024-06-11 17:50:02 +0200

    message-bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_100154) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_06_10_101756) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,7 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_101756) do
     t.index ["user_id"], name: "index_members_on_user_id"
   end
 
-<<<<<<< HEAD
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
@@ -86,8 +81,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_101756) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-=======
->>>>>>> master
   create_table "sports", force: :cascade do |t|
     t.string "name"
     t.string "category"
