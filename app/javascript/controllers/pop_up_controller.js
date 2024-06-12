68a76f01ee9daf46bafcec9149d795bbab18ab3a
Thomas Borg <thomaslborg7@gmail.com>
2024-06-12 12:54:57 +0200

    Confirmation popup added with sweetalert2

---
+++ app/javascript/controllers/pop_up_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+import Swal from 'sweetalert2';
+
+// Connects to data-controller="pop-up"
+export default class extends Controller {
+  connect() {
+  }
+
+  confirmation() {
+    Swal.fire({
+      icon: "success",
+      title: "Your request has been sent!",
+      text: "The activity will appear in your dashboard if it is accepted by the host!",
+      showCancelButton: true,
+      cancelButtonText: `<a href="/dashboard" style="color: white;">Open Dashboard</a>`,
+    })
+    this.element.remove()
+  }
+
+  approved() {
+    Swal.fire({
+      icon: "success",
+      title: "Request approved!",
+      text: "You can now chat with ",
+      showCancelButton: true,
+      cancelButtonText: `<a href="/chatroom" style="color: white;">Open Inbox</a>`,
+    })
+    this.element.remove()
+  }
+}
