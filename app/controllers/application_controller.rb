2650a698c7d5dbb13209fbf72b45257f7b11652d
theodorik comlan <comlantheodorik@gmail.com>
2024-06-12 02:17:55 +0200

    log in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name, :last_name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
