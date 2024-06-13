a7e6d9bf3e35de5057b03cc1d9f3a198cb3402dd
Lucas Silva <lucasilrs@outlook.com>
2024-06-13 09:42:35 +0200

    Animated Banner

---
+++ app/javascript/controllers/banner.js
@@ -0,0 +1,12 @@
+// app/javascript/controllers/banner.js
+// document.addEventListener("DOMContentLoaded", () => {
+  // const video = document.getElementById("youtube-video");
+
+  // document.addEventListener("visibilitychange", () => {
+  //   if (document.hidden) {
+  //     video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  //   } else {
+  //     video.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+  //   }
+  // });
+// });
